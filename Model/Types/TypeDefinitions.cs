4485be4e572bcc73e73f1e7bb808e6cc761ce5a4
fcurdi <franciscocurdi@gmail.com>
2020-05-11 08:55:09 -0300

    whitespace

--- Model/Types/TypeDefinitions.cs
+++ Model/Types/TypeDefinitions.cs
@@ -112,6 +112,7 @@ namespace Model.Types
 		public Constant Value { get; set; }
 
 		public bool IsStatic { get; set; }
+		
 		public bool SpecifiesRelativeVirtualAddress { get; set; }
 		public FieldDefinition(string name, IType type)
 		{
